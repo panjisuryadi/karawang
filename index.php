ae6d0915d08ac3ff8c1beb7903094b09bc614cf3
user <user@MacBook-Pro-user.local>
2025-07-24 16:36:29 +0700

    2

--- index.php
+++ index.php
@@ -86,51 +86,6 @@ $act	  = regis("act");
           <div class="navbar-vertical-content">
             <ul class="navbar-nav flex-column" id="navbarVerticalNav">
               <li class="nav-item">
-                <!-- parent pages-->
-                <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-home" role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="nv-home">
-                    <div class="d-flex align-items-center">
-                      <div class="dropdown-indicator-icon-wrapper"><span class="fas fa-caret-right dropdown-indicator-icon"></span></div><span class="nav-link-icon"><span data-feather="pie-chart"></span></span><span class="nav-link-text">Home</span>
-                    </div>
-                  </a>
-                  <div class="parent-wrapper label-1">
-                    <ul class="nav collapse parent show" data-bs-parent="#navbarVerticalCollapse" id="nv-home">
-                      <li class="collapsed-nav-item-title d-none">Home
-                      </li>
-                      <li class="nav-item"><a class="nav-link active" href="index.html">
-                          <div class="d-flex align-items-center"><span class="nav-link-text">E commerce</span>
-                          </div>
-                        </a>
-                        <!-- more inner pages-->
-                      </li>
-                      <li class="nav-item"><a class="nav-link" href="dashboard/project-management.html">
-                          <div class="d-flex align-items-center"><span class="nav-link-text">Project management</span>
-                          </div>
-                        </a>
-                        <!-- more inner pages-->
-                      </li>
-                      <li class="nav-item"><a class="nav-link" href="dashboard/crm.html">
-                          <div class="d-flex align-items-center"><span class="nav-link-text">CRM</span>
-                          </div>
-                        </a>
-                        <!-- more inner pages-->
-                      </li>
-                      <li class="nav-item"><a class="nav-link" href="dashboard/travel-agency.html">
-                          <div class="d-flex align-items-center"><span class="nav-link-text">Travel agency</span>
-                          </div>
-                        </a>
-                        <!-- more inner pages-->
-                      </li>
-                      <li class="nav-item"><a class="nav-link" href="apps/social/feed.html">
-                          <div class="d-flex align-items-center"><span class="nav-link-text">Social feed</span>
-                          </div>
-                        </a>
-                        <!-- more inner pages-->
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </li>
-              <li class="nav-item">
                 <!-- label-->
                 <p class="navbar-vertical-label">Apps
                 </p>
